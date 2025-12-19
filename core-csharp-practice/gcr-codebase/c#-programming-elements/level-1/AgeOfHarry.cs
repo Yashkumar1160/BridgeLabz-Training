53f3a5a45e62e9211ee3c525db9fcdeba7d2f160
Yashkumar1160 <yk4442144@gmail.com>
2025-12-19 23:11:41 +0530

    [Yash Kumar]add Finding Harry's age.

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/level-1/AgeOfHarry.cs
@@ -0,0 +1,13 @@
+using System;
+class AgeOfHarry{
+	static void Main(string[]args){
+		//Harry's birth year
+		int birthYear=2000;
+		//Current year
+		int currentYear=2024;
+		//Calculating Harry's age
+		int age=currentYear-birthYear;
+		//Output
+		Console.WriteLine("Harry's age in 2024 is "+age);
+	}
+}
