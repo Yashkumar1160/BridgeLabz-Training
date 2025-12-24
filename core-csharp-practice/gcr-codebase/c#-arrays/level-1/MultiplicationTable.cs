a1a10e67f86b88c221f332c34c855bcf65a85d91
Yashkumar1160 <yk4442144@gmail.com>
2025-12-24 14:57:57 +0530

    [Yash Kumar] Add Program to implement multiplication table using array

---
+++ core-csharp-practice/gcr-codebase/c#-arrays/level-1/MultiplicationTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.arrays.level_1
+{
+    internal class MultiplicationTable
+    {
+        public static void Main(string[] args)
+        {
+            //take integer number input
+            Console.WriteLine("Enter Number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //initialize array with length 10
+            int[] table = new int[10];
+
+            //store the results in the multiplication table
+            for (int i = 1; i <= 10; i++)
+            {
+                table[i - 1] = number * i;
+            }
+
+            //display the multiplication table
+            for (int i = 0; i < table.Length; i++)
+            {
+                Console.WriteLine(number + " * " + (i + 1) + " = " + table[i]);
+            }
+        }
+    }
+}
