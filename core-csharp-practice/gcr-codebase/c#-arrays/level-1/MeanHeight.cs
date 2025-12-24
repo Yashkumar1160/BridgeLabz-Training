85af2ee4987aa9fe1b7c84c42be287c87e228f78
Yashkumar1160 <yk4442144@gmail.com>
2025-12-24 14:56:24 +0530

    [Yash Kumar] Add Program to calculate mean height of students using array

---
+++ core-csharp-practice/gcr-codebase/c#-arrays/level-1/MeanHeight.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.arrays.level_1
+{
+    internal class MeanHeight
+    {
+        public static void Main(string[] args)
+        {
+            //create double array heights of size 11
+            double[] heights = new double[11];
+            //initialize sum variable as 0.0
+            double sum = 0.0;
+
+            for (int i = 0; i < heights.Length; i++)
+            {   //take height inputs
+                Console.WriteLine("Enter Height:");
+                heights[i] = Convert.ToDouble(Console.ReadLine());
+                //finding sum of all the heights
+                sum += heights[i];
+            }
+
+            //calculating mean height
+            double meanHeight = sum / 11;
+            Console.WriteLine("Mean height is :" + meanHeight);
+
+
+        }
+    }
+}
