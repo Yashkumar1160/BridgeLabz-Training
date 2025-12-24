bc9aa3f4e4ccc22bc4f7b9b1264c38d2bf1eca4c
Yashkumar1160 <yk4442144@gmail.com>
2025-12-24 14:55:03 +0530

    [Yash Kumar] Add Program to find all factors of a number using dynamic array resizing

---
+++ core-csharp-practice/gcr-codebase/c#-arrays/level-1/FactorsOfNumber.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.arrays.level_1
+{
+    internal class FactorsOfNumber
+    {
+        public static void Main(string[] args)
+        {
+            //take number input
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //initialize maxFactor variable to 1
+            int maxFactor = 10;
+            //initialize integer array factors with size maxFactor
+            int[] factors = new int[maxFactor];
+            //initialize index variable to 0
+            int index = 0;
+
+            //loop to find factors
+            for (int i = 1; i <= number; i++)
+            {
+                //check if i is a factor
+                if (number % i == 0)
+                {
+                    if (index == maxFactor)
+                    {   //if array is full then increase its size
+                        maxFactor = maxFactor * 2;
+
+                        //create temporary array
+                        int[] temp = new int[maxFactor];
+
+                        //copy all the elements to new array
+                        for (int j = 0; j < factors.Length; j++)
+                        {
+                            temp[j] = factors[j];
+                        }
+                        //assign new array
+                        factors = temp;
+                    }
+                    factors[index] = i;
+                    index++;
+                }
+            }
+            Console.WriteLine("Factors of " + number + " are: ");
+            for (int i = 0; i < index; i++)
+            {   //display factors of the number
+                Console.Write(factors[i] + " ");
+            }
+
+        }
+
+
+    }
+}
