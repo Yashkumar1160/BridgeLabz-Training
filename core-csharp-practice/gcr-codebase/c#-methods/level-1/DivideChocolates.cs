24890c7a0b9e9ac3d044df2fcf7573cd7a590542
Yashkumar1160 <yk4442144@gmail.com>
2025-12-25 21:36:13 +0530

    [Yash Kumar] Add chocolate distribution with quotient and remainder

---
+++ core-csharp-practice/gcr-codebase/c#-methods/level-1/DivideChocolates.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.c__methods.level_1
+{
+    internal class DivideChocolates
+    {
+        public static void Main(string[] args)
+        {
+            //take input for number of chocolates
+            Console.WriteLine("Enter number of chocolates:");
+            int numberOfChocolates = Convert.ToInt32(Console.ReadLine());
+            //take input for number of students
+            Console.WriteLine("Enter number of students:");
+            int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+
+            //call method
+            int[] result = FindRemainderAndQuotient(numberOfChocolates, numberOfStudents);
+
+            //display result
+            Console.WriteLine("Each child gets : " + result[0]);
+            Console.WriteLine("Remaining chocolates : " + result[1]);
+        }
+        //Method to find quotient and remainder
+        public static int[] FindRemainderAndQuotient(int numberOfChocolates, int numberOfStudents)
+        {
+            //create integer array to store distributed and remaining chocolates
+            int[] result = new int[2];
+            result[0] = numberOfChocolates / numberOfStudents;
+            result[1] = numberOfChocolates % numberOfStudents;
+            //return result array
+            return result;
+        }
+    }
+}
