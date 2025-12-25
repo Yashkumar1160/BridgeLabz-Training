da5396f0b57af5a7c4073661c9a38d784c2bd424
Yashkumar1160 <yk4442144@gmail.com>
2025-12-25 21:36:44 +0530

    [Yash Kumar] Add maximum number of handshakes calculation

---
+++ core-csharp-practice/gcr-codebase/c#-methods/level-1/MaxHandshakes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.c__methods.level_1
+{
+    internal class MaxHandshakes
+    {
+        public static void Main(string[] args)
+        {
+            //take input for number of student
+            Console.WriteLine("Enter number of students");
+            int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+
+            //create instance of class
+            MaxHandshakes maxHs = new MaxHandshakes();
+
+            //call the Handshakes() method
+            int totalHandshakes = maxHs.Handshakes(numberOfStudents);
+            //display result
+            Console.WriteLine("Maximum number of possible handshakes are : " + totalHandshakes);
+
+        }
+        //Method to find total number of possible handshakes
+        public int Handshakes(int numberOfStudents)
+        {
+            int totalHandshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+            //return total handshakes
+            return totalHandshakes;
+        }
+    }
+}
