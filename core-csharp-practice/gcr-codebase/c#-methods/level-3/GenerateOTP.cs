ae5e0aa95512160a86d40d84cf16da38fdada853
Yashkumar1160 <yk4442144@gmail.com>
2025-12-25 22:52:21 +0530

    [Yash Kumar] Add  GenerateOTP program to generate and check unique OTPs

---
+++ core-csharp-practice/gcr-codebase/c#-methods/level-3/GenerateOTP.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.c__methods.level_3
+{
+    internal class GenerateOTP
+    {
+        public static void Main(string[] args)
+        {
+            //create array to store otps
+            int[] otps = new int[10];
+            //loop to generate otps
+            for (int i = 0; i < otps.Length; i++)
+            {
+                otps[i] = Generate();
+                Console.WriteLine("OTP " + (i + 1) + " : " + otps[i]);
+            }
+            //check all otps are unique or not
+            Console.WriteLine("All OTPs unique: " + AreUnique(otps));
+        }
+        //Method to check unique otps
+        public static bool AreUnique(int[] otps)
+        {
+            //check uniqueness of otps
+            for (int i = 0; i < otps.Length; i++)
+            {
+                for (int j = i + 1; j < otps.Length; j++)
+                {
+                    if (otps[i] == otps[j])
+                    {   //if otps are not unique return false
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        //Method to generate otp
+        public static int Generate()
+        {   //create object of Random class
+            Random random = new Random();
+            //generate random otp
+            return random.Next(100000, 1000000);
+        }
+    }
+}
