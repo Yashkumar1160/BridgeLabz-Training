7392a93f812684bb0740835a765a1849889c2cc9
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:19:30 +0530

    [Yash Kumar] Add. Program to demonstrate IndexOutOfRangeException

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/level-1/IndexOutOfRange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_strings.level_1
+{
+    internal class IndexOutOfRange
+    {
+        public static void Main(string[] args)
+        {
+            //call method to generate and handle exception   
+            HandleException();
+
+        }
+        public static void HandleException()
+        {   //try block
+            try
+            {
+                string text = "hello";
+                Console.WriteLine(text[10]);
+            }
+            //catch block
+            catch (IndexOutOfRangeException)
+            {
+                //catch exception and display message
+                Console.WriteLine("IndexOutOfRangeException caught");
+            }
+        }
+    }
+}
