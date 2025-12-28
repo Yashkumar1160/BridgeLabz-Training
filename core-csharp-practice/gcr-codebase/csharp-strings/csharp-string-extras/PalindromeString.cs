df201b6d5a8e306c2d341ac1bef3133bd98cc4f2
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:27:32 +0530

    [Yash Kumar] Add. Program to check palindrome string

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/csharp-string-extras/PalindromeString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_strings.csharp_string_extras
+{
+    internal class PalindromeString
+    {
+        public static void Main(string[] args)
+        {
+            //take string input
+            Console.WriteLine("Enter a string");
+            string text = Console.ReadLine();
+            //call the CheckPalindrome method
+            string isPalindrome = CheckPalindrome(text) ? "Palindrome String" : "Not Palindrome";
+            Console.WriteLine(isPalindrome);
+        }
+        //Method to check palindrome string
+        public static bool CheckPalindrome(string text)
+        {
+            //initialize starting and ending index
+            int start = 0;
+            int end = text.Length - 1;
+            //loop to check characters
+            while (start <= end)
+            {
+                if (text[start] != text[end])
+                {
+                    return false;
+                }
+                //increment starting 
+                start++;
+                //decrement ending index
+                end--;
+            }
+            return true;
+        }
+    }
+}
