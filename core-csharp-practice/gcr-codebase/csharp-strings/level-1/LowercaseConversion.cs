5a06f25ce66c7d7584761b1848265cf58dd0acce
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:21:35 +0530

    [Yash Kumar] Add. Program to convert string to lowercase

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/level-1/LowercaseConversion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_strings.level_1
+{
+    internal class LowercaseConversion
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string text = Console.ReadLine();
+
+            //call the MakeLowercase method
+            string lowercase = MakeLowercase(text);
+            Console.WriteLine("Using created method : " + lowercase);
+            Console.WriteLine("Using inbuilt method : " + text.ToLower());
+
+        }
+        public static string MakeLowercase(string text)
+        {
+            //string to store lowercase letters
+            string lowercaseText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+                if (ch >= 'A' && ch <= 'Z')
+                {
+                    ch = (char)(ch + 32);
+                }
+                lowercaseText += ch;
+            }
+            return lowercaseText;
+        }
+    }
+}
