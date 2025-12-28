8d40cddc67b3e13627dc194cf5f45f7ddc96ab1d
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:18:39 +0530

    [Yash Kumar] Add. Program to demonstrate FormatException

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/level-1/FormatExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_strings.level_1
+{
+    internal class FormatExample
+    {
+        public static void Main(string[] args)
+        {
+            //call method to generate and handle exception
+            ExampleOfFormatException();
+
+        }
+        //Method to generate and handle FormatException
+        public static void ExampleOfFormatException()
+        {
+            //try block
+            try
+            {
+                //generate exception
+                int number = Convert.ToInt32("hello");
+
+            }
+            //catch block
+            catch (FormatException)
+            {
+                //catch and display exception
+                Console.WriteLine("FormatException caught");
+            }
+        }
+    }
+}
