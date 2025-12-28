138b353d09f3e4500afccd8ef4da806a769d08ed
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:17:22 +0530

    [Yash Kumar] Add. ArgumentOutOfRangeException example

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/level-1/ArgumentOutOfRange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_strings.level_1
+{
+    internal class ArgumentOutOfRange
+    {
+        public static void Main(string[] args)
+        {
+            //call the method to generate and handle exception
+            ExampleOfArgumentOutOfRange();
+        }
+        //Method to generate and handle ArgumentOutOfRangeException
+        public static void ExampleOfArgumentOutOfRange()
+        {
+            //try block
+            try
+            {
+                string text = "hello";
+                //generate exception
+                Console.WriteLine(text.Substring(4, 10));
+            }
+            //catch block
+            catch (ArgumentOutOfRangeException)
+            {
+                //catch and display exception
+                Console.WriteLine("ArgumentOutOfRangeException caught");
+            }
+        }
+    }
+}
