540abb35143961eb5415c71dce30c7aa193dc9a7
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:14:37 +0530

    [Yash Kumar] Add. Program to compare date

---
+++ core-csharp-practice/gcr-codebase/csharp-extras/csharp-datetime-built-in-functions/DateComparison.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_extras.csharp_datetime_built_in_functions
+{
+    internal class DateComparison
+    {
+        public static void Main(string[] args)
+        {
+            //take first date input
+            Console.WriteLine("Enter first date (yyyy-MM-dd)");
+            DateTime firstDate = Convert.ToDateTime(Console.ReadLine());
+
+            //take second date input
+            Console.WriteLine("Enter second date (yyyy-MM-dd)");
+            DateTime secondDate = Convert.ToDateTime(Console.ReadLine());
+
+            //Compare dates
+            int result = DateTime.Compare(firstDate, secondDate);
+
+            if (result < 0)
+            {
+                Console.WriteLine("First date is Before second date");
+            }
+            else if (result > 0)
+            {
+                Console.WriteLine("First date is After second date");
+            }
+            else
+            {
+                Console.WriteLine("Both dates are same");
+            }
+
+        }
+    }
+}
