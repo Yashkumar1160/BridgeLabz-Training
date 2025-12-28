415f028dcd862a8536b764c04327e348765004a1
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:07:49 +0530

    [Yash Kumar] Add. Program to calculate GCD and LCM

---
+++ core-csharp-practice/gcr-codebase/csharp-extras/csharp-built-in-functions/CalculateGcdAndLcm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_extras.csharp_built_in_functions
+{
+    internal class CalculateGcdAndLcm
+    {
+        public static void Main(string[] args)
+        {
+            //take first number input
+            Console.WriteLine("Enter first number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            //take second number input
+            Console.WriteLine("Enter second number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            //call FindGCD method and display result
+            int gcd = FindGCD(firstNumber, secondNumber);
+            Console.WriteLine("GCD is : " + gcd);
+            //call FindLCM method and display result
+            int lcm = FindLCM(firstNumber, secondNumber);
+            Console.WriteLine("LCM is : " + lcm);
+
+        }
+        //Method to find gcd
+        public static int FindGCD(int firstNumber, int secondNumber)
+        {   //loop until remainder becomes 0
+            while (secondNumber != 0)
+            {
+                int temp = secondNumber;
+                secondNumber = firstNumber % secondNumber;
+                firstNumber = temp;
+
+            }
+            return firstNumber;
+        }
+        //Method to find lcm
+        public static int FindLCM(int firstNumber, int secondNumber)
+        {
+            //calculate and return lcm
+            return (firstNumber/ FindGCD(firstNumber, secondNumber))*secondNumber;
+        }
+    }
+}
