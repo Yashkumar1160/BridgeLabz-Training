ecc41118ab0507f0c7daad07e139d68cd42e14e0
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:11:25 +0530

    [Yash Kumar] Add. Program for number guessing game

---
+++ core-csharp-practice/gcr-codebase/csharp-extras/csharp-built-in-functions/NumberGuessingGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_extras.csharp_built_in_functions
+{
+    internal class NumberGuessingGame
+    {
+        public static void Main(string[] args)
+        {
+            //tell user to think about a number
+            Console.WriteLine("Think of a number between 1 and 100");
+            //start number guessing game
+            StartGame();
+        }
+        //method to start number guessing game
+        public static void StartGame()
+        {   //high and low range
+            int low = 1;
+            int high = 100;
+            //string to collect feedback on every guess
+            string feedback;
+            //loop until number is guessed correctly
+            while (true)
+            {
+                //generate a number guess
+                int guess = GuessNumber(low, high);
+                //display guess and ask user to give feedback
+                Console.WriteLine("Is your number : " + guess);
+                Console.WriteLine("High/Low/Correct");
+                feedback = Console.ReadLine().ToLower();
+
+                if (feedback == "high")
+                {   //if guess is too high
+                    high = guess - 1;
+                }
+                else if (feedback == "low")
+                {   //if guess is too low
+                    low = guess + 1;
+                }
+                else
+                {   //if guess is correct
+                    Console.WriteLine("Computer guessed correct number");
+                    break;
+                }
+            }
+        }
+        //method to guess number
+        public static int GuessNumber(int low, int high)
+        {
+            return (low + high) / 2;
+        }
+    }
+}
