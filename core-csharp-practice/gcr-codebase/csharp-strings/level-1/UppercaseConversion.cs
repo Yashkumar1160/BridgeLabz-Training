b74f1fa664704ea2daa84658f8f926fe16275597
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:24:02 +0530

    [Yash Kumar] Add. Program to convert string to uppercase

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/level-1/UppercaseConversion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_strings.level_1
+{
+    internal class UppercaseConversion
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string text = Console.ReadLine();
+
+            //call the MakeUppercase method
+            string uppercase = MakeUppercase(text);
+            Console.WriteLine("Using created method : " + uppercase);
+            Console.WriteLine("Using inbuilt method : " + text.ToUpper());
+
+        }
+        //Method to convert text to uppercase
+        public static string MakeUppercase(string text)
+        {
+            //string to store uppercase letters
+            string uppercaseText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+                if (ch >= 'a' && ch <= 'z')
+                {
+                    ch = (char)(ch - 32);
+                }
+                uppercaseText += ch;
+            }
+            return uppercaseText;
+        }
+    }
+}
