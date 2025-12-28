85e10cc6a3ead48520df61f856fb313a39749b94
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:21:05 +0530

    [Yash Kumar] Add. Program to demonstrate Array IndexOutOfRangeException

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/level-1/IndexOutOfRangeExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_strings.level_1
+{
+    internal class IndexOutOfRangeExample
+    {
+        public static void Main(string[] args)
+        {
+            //call the method to generate and handle exception
+            ExampleOfIndexOutOfRange();
+        }
+        //Method to generate and handle IndexOutOfRangeException
+        public static void ExampleOfIndexOutOfRange()
+        {
+            //try block
+            try
+            {
+                int[] nums = { 1, 2, 3, 4 };
+                //generate exception
+                Console.WriteLine(nums[10]);
+            }
+            //catch block
+            catch (IndexOutOfRangeException)
+            {
+                //catch exception and display message 
+                Console.WriteLine("IndexOutOfRangeException caught");
+            }
+        }
+    }
+}
