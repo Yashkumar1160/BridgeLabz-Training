855594e60458afb74ca56acc109833f1ad04fb5c
Yashkumar1160 <yk4442144@gmail.com>
2025-12-28 22:29:09 +0530

    [Yash Kumar] Add. Program to reverse a string

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/csharp-string-extras/ReverseString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_strings.csharp_string_extras
+{
+    internal class ReverseString
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a string:");
+            string text = Console.ReadLine();
+            //call method to reverse string
+            Console.WriteLine(ReverseText(text));
+        }
+        //Method to reverse string
+        public static string ReverseText(string text)
+        {
+            //initialize empty string
+            string reverse = "";
+            //loop to add characters to reverse string
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reverse += text[i];
+            }
+            return reverse;
+        }
+    }
+
+}
