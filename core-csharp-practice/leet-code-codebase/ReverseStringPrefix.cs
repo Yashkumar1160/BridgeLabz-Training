f81f6b4858ac69af419d7200a81bf75b4913ae76
Yashkumar1160 <yk4442144@gmail.com>
2026-01-07 23:56:41 +0530

    [Yash Kumar] Add Reverse String Prefix Solution.

---
+++ core-csharp-practice/leet-code-codebase/ReverseStringPrefix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.leet_code_codebase
+{
+    internal class ReverseStringPrefix
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string s = Console.ReadLine();
+
+            Console.Write("Enter k: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            string result = ReversePrefix(s, k);
+            Console.WriteLine("Result: " + result);
+        }
+
+
+        static string ReversePrefix(string s, int k)
+        {
+            if (k <= 1 || k > s.Length)
+                return s;
+
+            char[] chars = s.ToCharArray();
+
+            int left = 0;
+            int right = k - 1;
+
+            while (left < right)
+            {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+
+                left++;
+                right--;
+            }
+
+            return new string(chars);
+        }
+    }
+}
