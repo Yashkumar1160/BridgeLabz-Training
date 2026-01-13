36cb43c4a27ca213ae9f9e0428962dcb3fcd95d6
Yashkumar1160 <yk4442144@gmail.com>
2026-01-13 21:36:08 +0530

    [Yash Kumar] Add program to reverse string

---
+++ dsa-csharp-practice/gcr-codebase/csharp-stringbuilder-stringbuffer/ReverseString.cs
@@ -0,0 +1,50 @@
+﻿using BridgeLabzTraining.oops_csharp_practice.gcr_codebase.csharp_encapsulation_inheritance_abstraction_polymorphism.ride_hailing_application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.dsa_csharp_practice.gcr_codebase.csharp_stringbuilder_stringbuffer
+{
+    internal class ReverseString
+    {
+        public static void Main(string[] args)
+        {
+            //take string input
+            Console.Write("ENTER A STRING: ");
+            string stringInput = Console.ReadLine();
+
+            //create object of StringBuilder
+            StringBuilder stringBuilder = new StringBuilder(stringInput);
+
+            //call FindReverseString method
+            FindReverseString(stringBuilder);
+        }
+
+
+        //method to reverse a string and display it 
+        public static void FindReverseString(StringBuilder stringBuilder)
+        {
+            //initialize start and end index
+            int start = 0;
+            int end = stringBuilder.Length - 1;
+
+            //loop until start index is less than end index
+            while (start < end)
+            {
+                //swap characters
+                char ch = stringBuilder[start];
+                stringBuilder[start] = stringBuilder[end];
+                stringBuilder[end] = ch;
+
+                start++;
+                end--;
+            }
+
+            //display reversed string
+            Console.WriteLine("REVERSED STRING: " + stringBuilder.ToString());
+
+        }
+    }
+}
