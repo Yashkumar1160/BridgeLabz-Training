7217bd76848667b7cbe8ebf02ee31375560b7bae
Yashkumar1160 <yk4442144@gmail.com>
2026-01-13 16:40:05 +0530

    [Yash Kumar] Add implementation of movie schedule manager

---
+++ oops-csharp-practice/scenario-based/cinema-time/IMovie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.cinema_time
+{
+    internal interface IMovie
+    {
+        void AddMovie(Movie movie);
+
+        void SearchMovie(string keyword);
+    }
+}
---
+++ oops-csharp-practice/scenario-based/cinema-time/MovieMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.cinema_time
+{
+    internal class MovieMain
+    {
+        public static void Main(string[] args)
+        {
+            MovieMenu.Start();
+        }
+    }
+}
