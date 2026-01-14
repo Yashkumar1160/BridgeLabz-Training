52dde2aa7f8a453b1797f75b7a085fb451ec1465
Yashkumar1160 <yk4442144@gmail.com>
2026-01-14 15:21:58 +0530

    [Yash Kumar] Add UC3: Added option to edit person contact

--- oops-csharp-practice/scenario-based/address-book/AddressBookMenu.cs
+++ oops-csharp-practice/scenario-based/address-book/AddressBookMenu.cs
@@ -19,8 +19,9 @@ namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.address_book
             while (true)
             {
                 Console.WriteLine("\n1. ADD NEW CONTACT");
-                Console.WriteLine("2. DISPLAY CONTACTS");
-                Console.WriteLine("3. EXIT PROGRAM");
+                Console.WriteLine("2. EDIT PERSON CONTACT");
+                Console.WriteLine("3. DISPLAY CONTACTS");
+                Console.WriteLine("4. EXIT PROGRAM");
                 Console.Write("ENTER YOUR CHOICE: ");
 
                 //take user's choice
@@ -31,52 +32,92 @@ namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.address_book
                 {
                     //add new contact
                     case 1:
+                        //if address book is full
+                        if (utility.TotalContacts == 99)
+                        {
+                            Console.WriteLine("ADDRESS BOOK IS FULL");
+                            return;
+                        }
+
+                        //take first name
                         Console.Write("ENTER FIRST NAME: ");
                         string firstName=Console.ReadLine();
 
+                        //take last name
                         Console.Write("ENTER LAST NAME: ");
                         string lastName =Console.ReadLine();
 
+                        //take email
                         Console.Write("ENTER EMAIL: ");
                         string email =Console.ReadLine();
                         
+                        //take city
                         Console.Write("ENTER CITY: ");
                         string city =Console.ReadLine();
                         
+                        //take state
                         Console.Write("ENTER STATE: ");
                         string state =Console.ReadLine();
                         
+                        //take zip code
                         Console.Write("ENTER ZIP CODE: ");
                         string zip =Console.ReadLine();
                         
+                        //take phone number
                         Console.Write("ENTER PHONE NUMBER: ");
                         string phoneNumber =Console.ReadLine();
                         
+                        //take address
                         Console.Write("ENTER ADDRESS: ");
                         string address =Console.ReadLine();
 
+                        //create object of Contact class
                         Contact contact = new Contact(firstName, lastName, address, city, state, zip, email, phoneNumber);
 
+                        //add new contact
                         utility.AddNewContact(contact);
 
-                        Console.WriteLine("CONTACT ADDED SUCCESSFULLY");
-
+                        Console.WriteLine("\nCONTACT ADDED SUCCESSFULLY");
                         break;
+
+
+                    //edit person contact
+                    case 2:
+                        //if no contact exists
+                        if (utility.TotalContacts == 0)
+                        {
+                            Console.WriteLine("\nNO CONTACT ADDED YET");
+                            break;
+                        }
+
+                        //take first name
+                        Console.Write("ENTER FIRST NAME: ");
+                        string personFirstName=Console.ReadLine();
+
+                        //take last name
+                        Console.Write("ENTER LAST NAME: ");
+                        string personLastName=Console.ReadLine();
+                        
+                        //call EditPersonalContact method
+                        utility.EditPersonContact(personFirstName, personLastName);
+                        break; 
 
 
                     //display contacts
-                    case 2:
+                    case 3:
                         utility.DisplayContacts();
                         break;
 
+
                     //exit program
-                    case 3:
-                        Console.WriteLine("EXIT SUCCESSFUL");
+                    case 4:
+                        Console.WriteLine("\nEXIT SUCCESSFUL");
                         return;
+
 
                     //invalid choice
                     default:
-                        Console.WriteLine("INVALID CHOICE");
+                        Console.WriteLine("\nINVALID CHOICE");
                         break;
                         
                 }
