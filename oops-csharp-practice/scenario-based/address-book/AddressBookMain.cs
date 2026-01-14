858b83dc13f32c678cc5818bc5c6797a6c4f2dfa
Yashkumar1160 <yk4442144@gmail.com>
2026-01-14 14:36:00 +0530

    [Yash Kumar] Add UC2: AddressBookMain class

---
+++ oops-csharp-practice/scenario-based/address-book/AddressBookMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.address_book
+{
+    internal class AddressBookMain
+    {
+        public static void Main(string[] args)
+        {
+            //call Start method to start program
+            AddressBookMenu.Start();
+        }
+    }
+}
