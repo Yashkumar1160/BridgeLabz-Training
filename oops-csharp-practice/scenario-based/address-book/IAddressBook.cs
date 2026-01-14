44d4d00c0bf6f25db6670f4ba04c3b8925debd01
Yashkumar1160 <yk4442144@gmail.com>
2026-01-14 14:38:58 +0530

    [Yash Kumar] Add UC2: IAddressBook interface with AddNewContact method.

---
+++ oops-csharp-practice/scenario-based/address-book/IAddressBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.address_book
+{
+    internal interface IAddressBook
+    {
+        //method to add new contact
+        void AddNewContact(Contact contact);
+
+    }
+}
