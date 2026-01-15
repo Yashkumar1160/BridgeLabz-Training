13b05175e9c1219c88abf156ccb4c3a2de1e1164
Yashkumar1160 <yk4442144@gmail.com>
2026-01-15 10:54:33 +0530

    [Yash Kumar] Add UC-6: Address Book class with fields- address book name and AddressBookUtilityImpl

---
+++ oops-csharp-practice/scenario-based/address-book/AddressBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.address_book
+{
+    internal class AddressBook
+    {
+        //variavle to store address book name
+        public string AddressBookName { get;  set; }
+
+        //variable to store contacts utility 
+        public ContactsUtilityImpl Utility { get;  set; }
+
+        //constructor
+        public AddressBook(string addressBookName)
+        {
+            AddressBookName = addressBookName;
+            Utility = new ContactsUtilityImpl();
+        }
+    }
+}
