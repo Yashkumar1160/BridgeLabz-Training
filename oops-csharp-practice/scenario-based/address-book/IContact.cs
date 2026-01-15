1805479714c74a69e07ecd75ac8c01901da3f2f4
Yashkumar1160 <yk4442144@gmail.com>
2026-01-15 11:02:40 +0530

    [Yash Kumar] Add UC-6: IContact containing different methods related to contacts

---
+++ oops-csharp-practice/scenario-based/address-book/IContact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.address_book
+{
+    internal interface IContact
+    {
+        //method to add new contact
+        void AddNewContact(Contact contact);
+
+        //method to edit person contact
+        void EditPersonContact(string firstName,string lastName);
+
+        //method to delete person contact
+        void DeletePersonContact(string firstName,string lastName);
+
+        //method to add mmultiple  contacts
+        void AddMultipleContacts(int numberOfContacts);
+    }
+}
