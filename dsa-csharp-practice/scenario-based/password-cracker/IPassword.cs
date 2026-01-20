7b6859a9033c7d0ef7fe65f349df5e13958885fb
Yashkumar1160 <yk4442144@gmail.com>
2026-01-20 16:06:39 +0530

    [Yash Kumar] Add password cracker simulator to crack password using recursion and backtracking

---
+++ dsa-csharp-practice/scenario-based/password-cracker/IPassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.dsa_csharp_practice.scenario_based.password_cracker
+{
+    internal interface IPassword
+    {
+        //method to set a password
+        void SetPassword();
+
+        //method to generate different passwords
+        void GeneratePasswords(int index, char[]attempt);
+
+        //method to start password cracking
+        void StartPasswordCracking();
+
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/password-cracker/PasswordMenu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.dsa_csharp_practice.scenario_based.password_cracker
+{
+    internal class PasswordMenu
+    {
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("-------PASSWORD CRACKER----------");
+
+            //create object of PasswordUtilityImpl class
+            PasswordUtilityImpl utility = new PasswordUtilityImpl();
+
+            //infinite loop until user exits
+            while(true)
+            {
+                Console.WriteLine("\n1. SET PASSWORD");
+                Console.WriteLine("2. START CRACKING");
+                Console.WriteLine("3. EXIT");
+                Console.Write("ENTER YOUR CHOICE: ");
+
+                //take user's choice
+                int choice=Convert.ToInt32(Console.ReadLine());
+
+                //handle user's choice
+                switch(choice)
+                {
+                    //set password
+                    case 1:
+                        utility.SetPassword();
+                        break;
+
+                    //start password cracking
+                    case 2:
+                        utility.StartPasswordCracking();
+                        break;
+
+                    //exit
+                    case 3:
+                        Console.WriteLine("\nExit Successful");
+                        return;
+
+                    //invalid choice
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+
+                }
+            }
+        }
+    }
+}
