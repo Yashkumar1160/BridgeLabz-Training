5ee08104675aeccb2897e347f6e663bc1b79f300
Yashkumar1160 <yk4442144@gmail.com>
2026-01-21 15:51:52 +0530

    [Yash Kumar] Add Super Market Billing System using queue and dictionary

---
+++ collections-csharp-practice/scenario-based/supermarket-billing/ISmartCheckout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.scenario_based.supermarket_billing
+{
+    internal interface ISmartCheckout
+    {
+        void AddItem();
+
+        void AddCustomer();
+
+        void GenerateBill();
+
+        double CalculateTotal(Customer customer);
+    }
+}
---
+++ collections-csharp-practice/scenario-based/supermarket-billing/SmartCheckoutMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.scenario_based.supermarket_billing
+{
+    internal class SmartCheckoutMain
+    {
+        public static void Main(string[] args)
+        {
+            SmartCheckoutMenu.DisplayMenu();
+        }
+    }
+}
