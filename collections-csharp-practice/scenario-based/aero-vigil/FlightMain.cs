bf4e50efae4cbae9a616f0b9a92c6abc0576fe74
Yashkumar1160 <yk4442144@gmail.com>
2026-02-01 22:42:25 +0530

    [Yash Kumar] Add Implementation of flight validation, passenger capacity checks, and fuel calculation with exception handling

---
+++ collections-csharp-practice/scenario-based/aero-vigil/FlightMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.scenario_based.aero_vigil
+{
+   internal class FlightMain
+   {
+       public static void Main(string[] args)
+       {
+           UserInterface.DisplayMenu();
+       }
+   }
+}
