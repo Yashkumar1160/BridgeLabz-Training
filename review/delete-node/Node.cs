662d83944fe422412bca14d0a1897d9a5f7b3f74
Yashkumar1160 <yk4442144@gmail.com>
2026-02-06 11:18:37 +0530

    [Yash Kumar] Add third review program to deleta a node in linked list without using head reference based on DSA

---
+++ review/delete-node/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace review.delete_node
+{
+    internal class Node
+    {
+        public int data;
+        public Node Next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.Next = null;
+        }
+    }
+}
---
+++ review/delete-node/NodeManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//delete nodes without using head reference
+namespace review.delete_node
+{
+    internal class NodeManager
+    {
+        Node head;
+        public void Add(Node newNode)
+        {
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node temp = head;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+
+            temp.Next = newNode;
+        }
+
+        public void DeleteNode(int deleteValue, Node[] nodes)
+        {
+            Node temp = nodes[0];
+
+            if (temp.data == deleteValue)
+            {
+                head = head.Next;
+                return;
+            }
+
+            Node prev = temp;
+            Node curr = temp.Next;
+            while (curr.data != deleteValue)
+            {
+                prev = prev.Next;
+                curr = curr.Next;
+            }
+
+            prev.Next = curr.Next;
+
+        }
+
+        public void Display()
+        {
+            Node temp = head;
+
+            while (temp != null)
+            {
+                Console.Write(temp.data + "  ");
+                temp = temp.Next;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            NodeManager list = new NodeManager();
+
+            Console.Write("Enter Number Of Nodes: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Node[] nodes = new Node[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter Number: ");
+                Node newNode = new Node(Convert.ToInt32(Console.ReadLine()));
+                list.Add(newNode);
+                nodes[i] = newNode;
+
+            }
+
+            list.Display();
+
+            Console.Write("\nEnter Data to be Deleted: ");
+            int delete = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nNode Deleted");
+
+            list.DeleteNode(delete, nodes);
+
+            list.Display();
+
+
+
+        }
+    }
+}
